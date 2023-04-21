b9a359b99deefa89bdd69cde33b3fb447c7d3da3
RiccardoSignorini <riccardo.signorini1@gmail.com>
2023-04-21 15:09:18 +0200

    apertura cartella lavoro

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+/*
+---PROBLEMA---
+Descrizione:
+Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+Bonus:
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+*/
+
+// INTESTAZIONE VUE
+const {createApp} = Vue
+
+createApp({
+    data() {
+        return{
+            // VARIABILI
+            
+        }
+    },
+
+    created() {
+
+    },	
+
+    methods: {
+        // FUNZIONI
+    }
+}).mount('#app')
